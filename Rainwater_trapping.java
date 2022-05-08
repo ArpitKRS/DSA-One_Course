d563def11fdeb9520086031dcb192b7d8d795da2
Arpit Kumar Singh <98815437+ArpitKRS@users.noreply.github.com>
2022-05-09 00:34:33 +0530

    Add files via upload

---
+++ Rainwater_trapping.java
@@ -0,0 +1,27 @@
+class Rainwater_trapping {
+    int rainWater(int a[]) {
+        int n = a.length;
+        int left[] = new int[n];
+        int right[] = new int[n];
+
+        left[0] = a[0];
+        for (int i = 1; i < n; i++) {
+            left[i] = Math.max(left[i - 1], a[i]);
+        }
+
+        right[n - 1] = a[n - 1];
+        for (int i = n - 1; i >= 0; i--) {
+            right[i] = Math.max(right[i + 1], a[i]);
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans += (Math.min(left[i], right[i]) - a[i]);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
