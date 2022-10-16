33035b7f40bf79bc61d23f330e76f9636d3c8f9d
Arpit Kumar Singh <98815437+ArpitKRS@users.noreply.github.com>
2022-10-17 00:11:28 +0530

    Create fibonacciRecurr.java

---
+++ fibonacciRecurr.java
@@ -0,0 +1,11 @@
+// This method is good, but it takes more space and time
+
+class Solution {
+  public int fib(int n) {
+    if(n<=1){
+      return n;
+    }
+    
+    return fib(n - 1) + fib(n + 1);
+  }
+}
